3507e39773f58b40e7e057fe666385c9baa2370b
AshaHopkins6 <ashaehopkins@gmail.com>
2022-09-21 00:47:30 -0500

    basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,38 @@
 import React from 'react';
 import {useForm, Resolver} from 'react-hook-form';
 import './App.css';
- 
+
 type FormValues = {
- username: string;
- password: string;
+  username: string;
+  password: string;
 };
- 
+
 const resolver: Resolver<FormValues> = async (values) => {
- return {
-   values: values.username ? values : {},
-   errors: !values.username
-     ? {
-         username: {
-           type: 'required',
-           message: 'Please Enter Username',
-         },
-       }
-     : {},
- };
+  return {
+    values: values.username ? values : {},
+    errors: !values.username
+      ? {
+          username: {
+            type: 'required',
+            message: 'Enter Username',
+          },
+        }
+      : {},
+  };
 };
- 
+
 export default function App() {
- const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({ resolver });
- const onSubmit = handleSubmit((data) => console.log(data));
- 
- return (
-   <form onSubmit={onSubmit}>
-     <div>
-       <input {...register("username")} placeholder="Username" />
-       {errors?.username && <p>{errors.username.message}</p>}
-     </div>
-     <div>
-       <input {...register("password")} placeholder="Password" />
-     </div>
-     <input type="submit" />
-   </form>
- );
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({ resolver });
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input {...register("username")} placeholder="Username" />
+      {errors?.username && <p>{errors.username.message}</p>}
+      
+      <input {...register("password")} placeholder="Password" />
+
+      <input type="submit" />
+    </form>
+  );
 }
-
-
-
